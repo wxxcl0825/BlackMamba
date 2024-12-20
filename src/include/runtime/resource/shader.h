0103d005623d39d08c1f9c1724405cc95ffb2d87
Chenli <wxxcl0825@126.com>
2024-12-20 17:18:43 +0800

    Adjust code style

--- src/include/runtime/resource/shader.h
+++ src/include/runtime/resource/shader.h
@@ -1,28 +1,28 @@
 #pragma once
 
 #include "common/common.h"
-#include "common/macro.h"
+
 #include <string>
-#include <fstream>
-#include <sstream>
 
-class Shader{
+class Shader {
 public:
-    Shader(const std::string& vertexPath, const std::string& fragmentPath);
-    ~Shader();
+  Shader(const std::string &vertexPath, const std::string &fragmentPath);
+  ~Shader();
 
-    void use() const;
+  void use() const;
 
-    unsigned int getProgramID() const { return _programID; }
+  GLuint getProgram() const { return _program; }
 
-    void setUniform(const std::string& name, float value) const;
-    void setUniform(const std::string& name, int value) const;
-    void setUniform(const std::string& name, glm::vec3 value) const;
-    void setUniform(const std::string& name, glm::mat3 value) const;
-    void setUniform(const std::string& name, glm::mat4 value) const;
+  void setUniform(const std::string &name, float value) const;
+  void setUniform(const std::string &name, int value) const;
+  void setUniform(const std::string &name, glm::vec3 value) const;
+  void setUniform(const std::string &name, glm::mat3 value) const;
+  void setUniform(const std::string &name, glm::mat4 value) const;
 
 private:
-    unsigned int _programID{0};
-    std::string _vertexPath;
-    std::string _fragmentPath;
+  enum class ErrorType { COMPILE, LINK };
+
+  GLuint _program{0};
+
+  static void checkShaderErrors(GLuint target, ErrorType type);
 };
